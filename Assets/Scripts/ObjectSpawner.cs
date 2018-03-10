7ac0da964d0fa8cd0cade6adf64763b4fb6015b8
Majak <majakthecoder@gmail.com>
2018-03-10 15:47:31 -0800

    working on lids, straws and putting drinks togetger

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject m_spawnPrefab;
+
+    public void DoAction()
+    {
+        var cook = Cook.Get();
+        if (!cook.Inventory.IsRightHandFree)
+            return;
+
+        var spawnedObject = Instantiate(m_spawnPrefab);
+        cook.GrabRightHand(spawnedObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
