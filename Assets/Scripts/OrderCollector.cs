8b6e7821062ec3dd33f24d46ca79800f92ce47c0
Majak <majakthecoder@gmail.com>
2018-03-12 23:47:36 -0700

    collecting orders wip

---
+++ Assets/Scripts/OrderCollector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderCollector : MonoBehaviour
+{
+    private Transform m_trayPlaceholder;
+
+    public void OnTouched()
+    {
+        if (!HasTray())
+        {
+            if (Cook.Get().IsHolding<Tray>())
+                Cook.Get().DropRightHand(m_trayPlaceholder);
+
+            return;
+        }
+    }
+
+    void Awake()
+    {
+        m_trayPlaceholder = transform.Find("TrayPlaceholder");
+    }
+
+    private bool HasTray()
+    {
+        return m_trayPlaceholder.GetComponentInChildren<Tray>() != null;
+    }
+
+    public void DispatchOrder()
+    {
+        if (!HasTray())
+            return;
+
+        Destroy(m_trayPlaceholder.GetChild(0).gameObject);
+    }
+}
---
+++ Assets/Scripts/Tray.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tray : MonoBehaviour
+{
+    private Transform m_burgerPlaceholder;
+    private Transform m_cupPlaceholder;
+
+    public void OnTouched()
+    {
+        if (Cook.Get().IsHolding<Burger>())
+            Cook.Get().DropRightHand(m_burgerPlaceholder);
+
+        if (Cook.Get().IsHolding<Cup>())
+            Cook.Get().DropRightHand(m_cupPlaceholder);
+    }
+
+    void Awake()
+    {
+        m_burgerPlaceholder = transform.Find("BurgerPlaceholder");
+        m_cupPlaceholder = transform.Find("CupPlaceholder");
+    }
+}
