27b26dfdc2fba208366688a27b9e2805ea7d68a3
Majak <majakthecoder@gmail.com>
2018-05-15 00:12:10 -0700

    wip

---
+++ Assets/Scripts/Ui/OrderList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderList : MonoBehaviour
+{
+    public OrderView m_orderViewPrefab;
+
+    public void AddOrder(int orderId, string text, float timeLimit, float reward)
+    {
+        var orderView = Instantiate(m_orderViewPrefab);
+        orderView.transform.SetParent(this.transform, false);
+        orderView.SetData(orderId, text, timeLimit, reward);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            AddOrder(0, "1. HAMBURGER\n\t- NO MEAT\n2. COCA COLA\n\t- NO ICE", Time.time + 2.0f, 4.79f);
+        }
+    }
+}
---
+++ Assets/Scripts/Ui/OrderView.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrderView : MonoBehaviour
+{
+    public Text m_textLabel;
+    public Text m_timeLabel;
+    public Text m_rewardLabel;
+    public Image m_bg;
+
+    public int OrderId
+    {
+        get;
+        private set;
+    }
+
+    public Color m_normalColor;
+    public Color m_warningColor;
+    public Color m_criticalColor;
+
+    private float m_timeLimit;
+
+    public void SetData(int orderId, string text, float timeLimit, float reward)
+    {
+        OrderId = orderId;
+        m_timeLimit = timeLimit;
+        m_textLabel.text = text;
+        m_rewardLabel.text = string.Format("${0:0.00}", reward);
+    }
+
+    void Update()
+    {
+        var timeSpan = System.TimeSpan.FromSeconds(m_timeLimit - Time.time);
+        if (timeSpan.TotalSeconds > 0)
+            m_timeLabel.text = string.Format("{0}:{1}", timeSpan.Minutes.ToString("D2"), timeSpan.Seconds.ToString("D2"));
+        else
+            m_timeLabel.text = "00:00";
+
+        if (timeSpan.TotalSeconds < 20.0f)
+            m_bg.color = m_criticalColor;
+        else if (timeSpan.TotalSeconds < 40.0f)
+            m_bg.color = m_warningColor;
+        else
+            m_bg.color = m_normalColor;
+    }
+}
