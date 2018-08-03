9e106578363d4c0050a6b56172e3a3b404721b36
Majak <majakthecoder@gmail.com>
2018-08-02 23:31:40 -0700

    bin animation

---
+++ Assets/Scripts/GarbageBin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageBin : MonoBehaviour
+{
+    private Animator m_animator;
+    private bool m_closed = true;
+
+    void Awake()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Touched()
+    {
+        if (m_closed)
+        {
+            m_animator.SetTrigger("Open");
+            m_closed = false;
+        }
+        else
+        {
+            m_animator.SetTrigger("Close");
+            m_closed = true;
+        }
+    }
+}
